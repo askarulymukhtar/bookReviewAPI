8d70b95b4ec0a37e0b5082fad05478b75b48c6ad
Mukhtar <askarulymukhtar@gmail.com>
2023-10-28 10:22:33 +0600

    changed book handler funcs to go routine

--- handlers/handler_books.go
+++ handlers/handler_books.go
@@ -12,50 +12,101 @@ import (
 )
 
 func HandleGetAll(w http.ResponseWriter, r *http.Request) {
+	type result struct {
+		books []models.BookDTO
+		err   error
+	}
 
-	rows, err := db.DB.Query("SELECT title, author, publication_year, genre, isnb FROM books")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	var books []models.BookDTO
-	for rows.Next() {
-		var i models.BookDTO
-		if err = rows.Scan(&i.Title, &i.Author, &i.PublicationYear, &i.Genre, &i.ISBN); err != nil {
-			log.Fatal(err)
+	// Create a channel to communicate the results from the goroutine
+	resultChannel := make(chan result)
+
+	go func() {
+		rows, err := db.DB.Query("SELECT title, author, publication_year, genre, isbn FROM books")
+		if err != nil {
+			resultChannel <- result{err: err}
+			return
 		}
-		books = append(books, i)
+		defer rows.Close()
+
+		var books []models.BookDTO
+		for rows.Next() {
+			var i models.BookDTO
+			if err = rows.Scan(&i.Title, &i.Author, &i.PublicationYear, &i.Genre, &i.ISBN); err != nil {
+				resultChannel <- result{err: err}
+				return
+			}
+			books = append(books, i)
+		}
+		resultChannel <- result{books: books}
+	}()
+
+	res := <-resultChannel
+	if res.err != nil {
+		log.Printf("error while getting books: %v", res.err)
+		http.Error(w, res.err.Error(), http.StatusInternalServerError)
+		return
 	}
-	json.NewEncoder(w).Encode(books)
+
+	json.NewEncoder(w).Encode(res.books)
 }
 
 func HandleGetById(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 
-	var book models.BookDTO
+	// Define a result struct to hold the query results
+	type result struct {
+		book models.BookDTO
+		err  error
+	}
 
-	if err := db.DB.QueryRow("SELECT title, author, publication_year, genre, isnb FROM books WHERE id = $1", id).Scan(
-		&book.Title, &book.Author, &book.PublicationYear, &book.Genre, &book.ISBN); err != nil {
-		log.Fatal(err)
+	// Create a channel to communicate results
+	resultChannel := make(chan result)
+
+	// Execute the query in a goroutine
+	go func() {
+		var book models.BookDTO
+		err := db.DB.QueryRow("SELECT title, author, publication_year, genre, isbn FROM books WHERE id = $1", id).Scan(
+			&book.Title, &book.Author, &book.PublicationYear, &book.Genre, &book.ISBN)
+
+		// Send the results back to the main goroutine
+		resultChannel <- result{book: book, err: err}
+	}()
+
+	// Receive the results
+	res := <-resultChannel
+
+	// Handle the error if any
+	if res.err != nil {
+		log.Printf("error while getting book: %v", res.err)
+		http.Error(w, "could not retrieve book", http.StatusInternalServerError)
 		return
 	}
 
+	// Set the content type and write the response
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(book)
+	json.NewEncoder(w).Encode(res.book)
 }
 
 func HandleCreate(w http.ResponseWriter, r *http.Request) {
 	params := models.BookDTO{}
 	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		log.Fatal("error marshalling json")
+		log.Printf("error while parsing json: %v", err)
 		return
 	}
 
-	if err := db.DB.QueryRow("INSERT INTO books(id, title, author, publication_year, genre, isnb) VALUES ($1, $2, $3, $4, $5, $6)",
-		uuid.New(), params.Title, params.Author, params.PublicationYear, params.Genre, params.ISBN).Err(); err != nil {
+	resultChannel := make(chan error)
+
+	go func() {
+		err := db.DB.QueryRow("INSERT INTO books(id, title, author, publication_year, genre, isbn) VALUES ($1, $2, $3, $4, $5, $6)",
+			uuid.New(), params.Title, params.Author, params.PublicationYear, params.Genre, params.ISBN).Err()
+		resultChannel <- err
+	}()
+
+	err := <-resultChannel
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		log.Fatal(err)
+		log.Printf("error while creating book: %v", err)
 		return
 	}
 
@@ -67,14 +118,22 @@ func HandleUpdate(w http.ResponseWriter, r *http.Request) {
 	params := models.BookDTO{}
 	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		log.Fatal("error marshalling json")
+		log.Printf("error while parsing json: %v", err)
 		return
 	}
 
-	if err := db.DB.QueryRow("UPDATE books SET title=$2, author=$3, publication_year=$4, genre=$5, isnb=$6 WHERE id=$1",
-		id, params.Title, params.Author, params.PublicationYear, params.Genre, params.ISBN).Err(); err != nil {
+	resultChannel := make(chan error)
+
+	go func() {
+		err := db.DB.QueryRow("UPDATE books SET title=$2, author=$3, publication_year=$4, genre=$5, isbn=$6 WHERE id=$1",
+			id, params.Title, params.Author, params.PublicationYear, params.Genre, params.ISBN).Err()
+		resultChannel <- err
+	}()
+
+	err := <-resultChannel
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		log.Fatal(err)
+		log.Printf("error while updating book: %v", err)
 		return
 	}
 
@@ -84,9 +143,17 @@ func HandleUpdate(w http.ResponseWriter, r *http.Request) {
 func HandleDelete(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 
-	if err := db.DB.QueryRow("DELETE FROM books WHERE id=$1", id).Err(); err != nil {
+	resultChannel := make(chan error)
+
+	go func() {
+		err := db.DB.QueryRow("DELETE FROM books WHERE id=$1", id).Err()
+		resultChannel <- err
+	}()
+
+	err := <-resultChannel
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		log.Fatal(err)
+		log.Printf("error while deleting book: %v", err)
 		return
 	}
 
